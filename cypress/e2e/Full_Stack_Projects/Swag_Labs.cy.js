acf48b301c70d344b24659245bd50cb341f8f423
Abhinandan Mishra <abhi.nandanmishra@outlook.com>
2026-09-17 06:35:50 +0530

    Learning to commit --Added Swag Labs test and fixed login selector

---
+++ cypress/e2e/Full_Stack_Projects/Swag_Labs.cy.js
@@ -0,0 +1,9 @@
+describe('Swag Labs', () => {
+  it('Open the website', () => {
+    cy.visit('https://www.saucedemo.com/');
+  });
+  it('Login with valid credentials', () => {
+    cy.get()
+
+  });
+});
--- cypress/e2e/StartingSteps/Access&click.cy.js
+++ cypress/e2e/StartingSteps/Access&click.cy.js
@@ -4,7 +4,7 @@ describe('siteAccess', () => {
         cy.get('#user-name').type('standard_user')
         cy.get('#password').type('secret_sauce')
         cy.get('#login-button').click()
-        cy.get(#add-to-cart-sauce-labs-backpack').click()
+        cy.get('#add-to-cart-sauce-labs-backpack').click()
         cy.get('.product_sort_container').select('AZ')
         cy
     })
