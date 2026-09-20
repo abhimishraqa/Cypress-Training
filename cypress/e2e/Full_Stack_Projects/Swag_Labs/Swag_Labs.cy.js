1202a4ff0bac2c28ae0cdfaa6eb113b501866e95
Abhinandan Mishra <abhi.nandanmishra@outlook.com>
2026-09-20 10:41:49 +0530

    Added steps till listing the cart details in log

--- cypress/e2e/Full_Stack_Projects/Swag_Labs/Swag_Labs.cy.js
+++ cypress/e2e/Full_Stack_Projects/Swag_Labs/Swag_Labs.cy.js
@@ -1,5 +1,8 @@
 describe('Swag Labs', () => {
-  it('Open the website', () => {
+  it('Swag Labs Happy Flow', () => {
+
+      // Visit the website
+
     cy.visit('https://www.saucedemo.com/');
       
       //Login to the website
@@ -23,7 +26,8 @@ describe('Swag Labs', () => {
     cy.get('.product_sort_container') .select('lohi');
     cy.get('#add-to-cart-sauce-labs-fleece-jacket').click();
 
-      //Check the cart badge; print in console. 
+      //Check the cart badge; print in console and apply chai assertion with expected value.
+
     cy.get('[data-test="shopping-cart-badge"]') .then((badge) => {
       // If we need to use the badge count just as a string, we can use:-
       cy.log('No of products in the cart = ' + badge.text());
@@ -31,24 +35,61 @@ describe('Swag Labs', () => {
       /*if have to use the badge count as a number, we can use:-
       const count = parseInt(badge.text());
       cy.log('No of products in the cart as number = ' + count);*/
+
+      expect(badge.text()).to.eq('3');
     })
       
       // Remove 2 products from the cart.
-    
-      //Check the cart badge; print in console.
+
+    cy.get('#remove-sauce-labs-backpack').click();
+    cy.get('#remove-sauce-labs-onesie').click();
+
+      //Check the cart badge; print in console and apply chai assertion with expected value.
       
+    cy.get('[data-test="shopping-cart-badge"]') .then((badge) => {
+      /* If we need to use the badge count just as a string, we can use:-
+      cy.log('No of products in the cart = ' + badge.text());*/
+
+      //if have to use the badge count as a number, we can use:-
+      const count = parseInt(badge.text());
+      cy.log('No of products in the cart as number = ' + count);
+
+      expect(badge.text()).to.eq('1');
+    })
+
       //Open a product detail page.
+
+    cy.contains('.inventory_item_label', 'Sauce Labs Bike Light')
+      .find('.inventory_item_name').click();
       
       //Verify the title of the page; Print in console.
       
+    cy.title().then((title) => {
+      cy.log('Current page title = ' + title);
+      expect(title).to.eq('Swag Labs');
+    })
+
       //Add the product from the detail page to the cart
       
+    cy.contains('button', 'Add to cart').click();
+
       //Check the cart badge; print in console.
+
+    cy.get('[data-test="shopping-cart-badge"]') .then((badge) => {
+      const count = parseInt(badge.text())
+      cy.log('No of products in the cart = ' + badge.text());
+      })
       
-      //Go to cart page
+      //Go to product list page and then cart page
     
+    cy.get('#back-to-products').click();
+    cy.get('[data-test="shopping-cart-link"]').click();
+
       //Verify the title of the page; Print in console.
-      
+
+    cy.get('.cart_item') .each((product) => {
+      cy.log(product.text()); 
+    });
       // Go to checkout
     
       // Fill the details
